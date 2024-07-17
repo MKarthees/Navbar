4ac1c0a27bd535bb24356109b2411c8c577b8e8d
MKarthees <kartheeswaran68@gmail.com>
2024-07-17 23:42:11 +0530

    Navbar

---
+++ main.js
@@ -0,0 +1,8 @@
+var menuicon = document.querySelector(".fa fa-bars");
+var overlay = document.querySelector(".overlay");
+var menu = document.querySelector(".menu-container");
+
+function show() {
+  overlay.style.display = "block";
+  menu.style.display = "block";
+}
