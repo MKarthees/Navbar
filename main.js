5f04f3af64203c1c1b46e842edeb9b6b89f79611
MKarthees <kartheeswaran68@gmail.com>
2024-07-18 18:25:12 +0530

    Update

--- main.js
+++ main.js
@@ -1,8 +1,14 @@
 var menuicon = document.querySelector(".fa fa-bars");
 var overlay = document.querySelector(".overlay");
 var menu = document.querySelector(".menu-container");
+var cancle = document.querySelector(".cancle");
 
 function show() {
   overlay.style.display = "block";
   menu.style.display = "block";
+}
+
+function unshow() {
+  overlay.style.display = "none";
+  menu.style.display = "none";
 }
